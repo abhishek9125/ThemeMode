5f538053304eabf42d00c03f66a35433a0a137da
abhishek9125 <abhishek4075@gmail.com>
2024-09-30 18:16:52 +0530

    Created Theme Mode

---
+++ src/pages/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Blog() {
+    return (
+        <div className='page'>
+            <h1>Blog</h1>
+            <p>Welcome to Blog Page</p>
+        </div>
+    )
+}
+
+export default Blog
